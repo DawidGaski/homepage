8415eb789bf2f0f6f90d111a016abc0c64df5cc6
Dawid Galeski <dawidgaski@gmail.com>
2023-03-14 16:03:26 +0100

    shortening code in script.js

--- javascript/script.js
+++ javascript/script.js
@@ -3,34 +3,33 @@
         console.log("hi!")
     };
 
-    hi();
-
     const toggleBackground = () => {
         const body = document.querySelector(".body");
-        const header__themeName = document.querySelector(".js-themeName");
+        const themeName = document.querySelector(".js-themeName");
+
         body.classList.toggle("changeBackground");
-        if (body.classList.contains("changeBackground")) {
-            header__themeName.innerText = "ciemne";
-        } else {
-            header__themeName.innerText = "jasne";
-        }
+
+        themeName.innerHTML = body.classList.contains("changeBackground") ? "ciemne" : "jasne";
     };
 
     const toggleTextColor = () => {
         const body = document.querySelector(".body");
-        const header__secondThemeName = document.querySelector(".js-secondThemeName");
+        const secondThemeName = document.querySelector(".js-secondThemeName");
+
         body.classList.toggle("changeText");
 
-        header__secondThemeName.innerText = body.classList.contains("changeText") ? "Wyłącz" : "Włącz";
+        secondThemeName.innerText = body.classList.contains("changeText") ? "Wyłącz" : "Włącz";
 
     };
 
     const init = () => {
-        const header__button = document.querySelector(".js-button");
-        const header__secondButton = document.querySelector(".js-secondButton");
+        const button = document.querySelector(".js-button");
+        const secondButton = document.querySelector(".js-secondButton");
 
-        header__button.addEventListener("click", toggleBackground);
-        header__secondButton.addEventListener("click", toggleTextColor);
+        button.addEventListener("click", toggleBackground);
+        secondButton.addEventListener("click", toggleTextColor);
+
+        hi();
     };
     init();
 }
